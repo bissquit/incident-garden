4bcd21d92ce722457786ab068dd2dbebee93eb1e
Egor Vasilyev <e.s.vasilyev@mail.ru>
2026-01-22 20:04:30 +0300

    fix linters

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,14 @@
+// Package version contains build version information.
 package version
 
+// Version is the current application version.
+// This value is updated automatically by Release Please.
 var Version = "0.0.0"
 
+// GitCommit is the git commit hash.
+// This value is set at build time via ldflags.
 var GitCommit = "unknown"
 
+// BuildDate is the build date.
+// This value is set at build time via ldflags.
 var BuildDate = "unknown"
